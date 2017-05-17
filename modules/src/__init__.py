add45372ffc07d0c20046b3fe9627712ae9cea15
luxory <dovescrymgmt@gmail.com>
2017-05-17 14:22:14 -0400

    init

---
+++ modules/src/__init__.py
@@ -0,0 +1,33 @@
+# List of all modules
+__all__ = [
+    'anime',
+    'book',
+    'bye',
+    'coin',
+    'currency',
+    'dice',
+    'dictionary',
+    'fact',
+    'hello',
+    'help',
+    'joke',
+    'lyrics',
+    'movie',
+    'music',
+    'news',
+    'quote',
+    'request',
+    'time',
+    'url',
+    'video',
+    'weather',
+    'wiki',
+    'xkcd',
+]
+
+# List of modules that send data personalized to the user
+__personalized__ = [
+    'bye',
+    'hello',
+    'help',
+]
