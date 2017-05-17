59b41ec61d82e27280b7a091d18c491c9bb059eb
luxory <dovescrymgmt@gmail.com>
2017-05-17 14:49:16 -0400

    xkcd support

---
+++ modules/tests/test_xkcd.py
@@ -0,0 +1,6 @@
+import modules
+
+def test_xkcd():
+    assert('xkcd' == modules.process_query('xkcd comic')[0])
+    assert('xkcd' == modules.process_query('xkcd')[0])
+    assert('xkcd' != modules.process_query('something random')[0])
