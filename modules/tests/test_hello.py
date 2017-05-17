acad0f0a5c9fadef809093a65d48946eb30bfde8
luxory <dovescrymgmt@gmail.com>
2017-05-17 14:48:37 -0400

    henlo fren

---
+++ modules/tests/test_hello.py
@@ -0,0 +1,7 @@
+import modules
+
+def test_hello():
+    assert('hello' == modules.process_query('hello')[0])
+    assert('hello' == modules.process_query('Hi, Rero!')[0])
+    assert('hello' == modules.process_query('Are you there?')[0])
+    assert('hello' != modules.process_query('something random')[0])
