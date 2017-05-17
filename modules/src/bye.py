0fe72f50432bb86bcf77806b16f50b695b0e1b3c
luxory <dovescrymgmt@gmail.com>
2017-05-17 14:22:35 -0400

    bye.py

---
+++ modules/src/bye.py
@@ -0,0 +1,22 @@
+import random
+from templates.text import TextTemplate
+
+def process(input, entities=None):
+    greetings = [
+        'Have a good day, sir.',
+        'Wonderful, I think it\'s time for my evening nap...',
+        'Bye to you as well, sir.',
+        'It was my pleasure talking to you.',
+        'Oh, please do not go!',
+		' cya nibba',
+    ]
+    if entities is not None:
+        if 'sender' in entities and 'first_name' in entities['sender']:
+            sender_name = entities['sender']['first_name']
+            greetings = [greeting.replace('sir', sender_name) for greeting in greetings]
+    output = {
+        'input': input,
+        'output': TextTemplate(random.choice(greetings)).get_message(),
+        'success': True
+    }
+    return output
