10fd26a69ab028dd0170f185091b8252edd02890
luxory <dovescrymgmt@gmail.com>
2017-05-17 19:36:21 -0400

    send help

---
+++ modules/src/help.py
@@ -0,0 +1,35 @@
+from templates.text import TextTemplate
+
+def process(input, entities=None):
+    help = 'Hi there! I\'m Rero, your personal assistant.'
+    if entities is not None:
+        if 'sender' in entities and 'first_name' in entities['sender']:
+            sender_name = entities['sender']['first_name']
+            help = help.replace('there', sender_name)
+    help += '\n\nYou can tell me things like:'
+    help += '\n  - define comfort'
+    help += '\n  - superbad movie plot'
+    help += '\n  - tell me a joke/quote/fact'
+    help += '\n  - wiki html'
+    help += '\n  - anything you want book'
+    help += '\n  - usd to eur rate'
+    help += '\n  - death note anime'
+    help += '\n  - time in seattle'
+    help += '\n  - songs by elton john'
+    help += '\n  - shorten google.com'
+    help += '\n  - weather in london'
+    help += '\n  - videos of taylor'
+    help += '\n  - flip a coin'
+    help += '\n  - roll a die'
+    help += '\n  - show a random xkcd comic'
+    help += '\n  - latest news'
+    help += '\n  - paradise lyrics'
+    help += '\n\nI\'m always learning, so do come back and say hi from time to time!'
+    help += '\nHave a nice day. :)'
+
+    output = {
+        'input': input,
+        'output': TextTemplate(help).get_message(),
+        'success': True
+    }
+    return output
